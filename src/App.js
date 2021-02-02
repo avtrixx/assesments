069418fbb32c72056645f953bcf11371f8542793
avtrixx <avtrixx@gmail.com>
2021-02-02 16:54:38 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyPage  from "./MyPage";
+
+const  App= ()=> {
+  return (
+    <div className= 'App'>
+       <MyPage></MyPage>
+    </div>
+  );
+}
+
+export default App;
